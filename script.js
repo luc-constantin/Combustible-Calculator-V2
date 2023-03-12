44a8b2f52570fcb915ae3ffb12d79093ceea63ae
Luc Constantin <lucc.constantin@gmail.com>
2023-03-12 18:49:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const form = document.querySelector('#combustible-form');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  const distance = parseFloat(document.querySelector('#distance').value.replace(/,/g, ''));
-  const price = parseFloat(document.querySelector('#price').value.replace(/,/g, ''));
-  const consumption = parseFloat(document.querySelector('#consumption').value.replace(/,/g, ''));
+  const distance = parseFloat(document.querySelector('#distance').value.replace(/,(?!\d)/g, ''));
+  const price = parseFloat(document.querySelector('#price').value.replace(/,(?!\d)/g, ''));
+  const consumption = parseFloat(document.querySelector('#consumption').value.replace(/,(?!\d)/g, ''));
   const cost = (distance / 100) * consumption * price;
   const resultDiv = document.querySelector('#result');
   resultDiv.innerHTML = `Result: The total cost of combustible used for ${distance.toLocaleString()} kilometers is ${cost.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} Euros (€).`;
@@ -13,31 +13,31 @@ form.addEventListener('submit', function(event) {
   }, 30000);
 });
 
-
 function CalculateNew() {
-    // var msg;
-    // msg= " Recalculate?";
-    // var agree=confirm(msg);
-    // if (agree)
-    return window.location.reload();
-    // else
-    // return false;
-    }
+  // var msg;
+  // msg= " Recalculate?";
+  // var agree=confirm(msg);
+  // if (agree)
+  return window.location.reload();
+  // else
+  // return false;
+}
 
-	const calculateBtn = document.querySelector('#calculateBtn');
+const calculateBtn = document.querySelector('#calculateBtn');
 
-	function toggleButtonColor() {
-	  const distance = document.querySelector('#distance').value;
-	  const price = document.querySelector('#price').value;
-	  const consumption = document.querySelector('#consumption').value;
-	  
-	  if (distance && price && consumption) {
-		calculateBtn.style.backgroundColor = '#9966ff';
-	  } else {
-		calculateBtn.style.backgroundColor = '';
-	  }
-	}
-	
-	document.querySelectorAll('input').forEach(input => {
-	  input.addEventListener('input', toggleButtonColor);
-	});
+function toggleButtonColor() {
+  const distance = document.querySelector('#distance').value;
+  const price = document.querySelector('#price').value;
+  const consumption = document.querySelector('#consumption').value;
+  
+  if (distance && price && consumption) {
+    calculateBtn.style.backgroundColor = '#9966ff';
+  } else {
+    calculateBtn.style.backgroundColor = '';
+  }
+}
+
+document.querySelectorAll('input').forEach(input => {
+  input.addEventListener('input', toggleButtonColor);
+});
+
